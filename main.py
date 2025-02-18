dad0da68b87a1f80dc22340c73c060e7d55eceda
annaaa11 <gosberex@gmail.com>
2025-02-18 17:28:35 +0200

    task of Homework 18/02/25: напишіть клас Банківський рахунок з атрибутами

--- main.py
+++ main.py
@@ -1,171 +1,125 @@
 
 
-# 1. Створіть клас Cart(кошик клієнта магазину) з атрибутами
-#client(ім’я клієнта) та items(список товарів).
-#Додайте метод який додає новий товар до кошика
-#Додайте метод який видаляє товар з кошика
-#Додайте метод для виведення інформації про кошик
+##BANK###
 
-class Cart:
-    def __init__(self, client):
-        self.client = client
-        self.items = []
-
-    def add_item(self):
-        self.items.append(input("новий товар: "))
-
-    def clear(self):
-        self.items.clear()
-
-
-    def print_items(self):
-        print(f"список товарів: {self.items} клієнта магазину {self.client}")
-
-
-cart1 = Cart(client = input("ім’я клієнта: "))
-
-cart1.add_item()
-cart1.add_item()
-
-cart1.print_items()
-cart1.clear()
-cart1.print_items()
-
-# 2.Створіть клас Phone з атрибутами number та battery_level.
-#Додайте метод який зменшує заряд телефона(на скільки
-#зменшити відсотків передається як параметр), якщо він
-#опуститься нижче 20%, вивести повідомлення
-#Додайте метод для виведення інформації про телефон.
-
-class Phone():
-    def __init__(self, number, battery_level):
-        self.number = number
-        self.battery_level = battery_level
-
-
-    def low_balance(self, procent):
-        self.battery_level -= procent
-        if self.battery_level < 20:
-            print("заряд телефона опуститься нижче 20%")
-
-    def print_info(self):
-        print(f"заряд телефона {self.number}  складає {self.battery_level} %")
-
-
-phone1 = Phone("098889900", 100)
-phone1.low_balance(82)
-phone1.print_info()
-
-# Завдання 1
-# Створіть клас Проект з атрибутами:
-#  назва
-#  виділений кошторис
-#  загальні витрати
-#  чи завершений(за замовчуванням False)
-#  час виконання(за замовчуванням 0 місяців)
-#  список необхідних задач
+# Напишіть клас Банківський рахунок з атрибутами:
+#  ім'я клієнта
+#  баланс
+#  валюта
+#  словник з курсом валют(однаковий для всіх)
 # Додайте методи:
-#  вивід інформації: назва, час виконання, необхідні
-# задачі
-#  добавити нову задачу
-#  розбити задачу на під-задачі: передається назва задачі
-# та список під-задач
-#  виконати задачу, передається назва, час та ціна
-# виконання
-#  поповнення кошторису
+#  вивід загальної інформації
+#  перевірка чи відома валюта(якщо ні, викликати
+# ValueError)
+#  перевести гроші з однієї валюти в іншу(ця операція
+# часто використовується, тому зрочно реалізувати
+# окремим методом)
+#  зміна валюти
+#  поповнення балансу(валюта та сама)
+#  зняття грошей з балансу(валюта та сама).
 
 
-class Project:
-    def __init__(self, name, budget, tasks):
-        self.name = name
-        self.budget = budget
-        self.tasks = tasks
+class BankAccount:
+    # словник курсів валют (однаковий для всіх)
+    exchange_rates = {
+        "UAH": 1,  # базова валюта
+        "USD": 40,  # 1 USD = 40 UAH
+        "EUR": 44,  # 1 EUR = 44 UAH
+        "GBP": 50  # 1 GBP = 50 UAH
+    }
 
-        self.expenses = 0
-        self.is_finished = False
-        self.spent_time = 0  # місяці
+    def __init__(self, client_name, balance, currency):
+        self.client_name = client_name
+        self.balance = balance
+        self.currency = currency.upper()
+        self.check_currency(self.currency)  # Перевірка, чи валюта відома
+
+    def check_currency(self, currency):
+        """Перевіряє, чи вказана валюта присутня у словнику курсів."""
+        if currency.upper() not in BankAccount.exchange_rates:
+            raise ValueError(f"Невідома валюта: {currency}.")
+        #return True
 
     def display_info(self):
-        print(f"Проєкт {self.name}")
-        print(f'Час виконання {self.spent_time} місяців')
-        print(f'Витрачено {self.expenses}/{self.budget}$')
-        print(f'Залишилось {self.budget - self.expenses}$')
 
-        if self.is_finished: # якщо проект завершений
-            print("Стан проекту: завершений")
+        print(f"клієнт: {self.client_name}")
+        print(f"Баланс: {self.balance:.2f} {self.currency}")
+        print("Курси валют (до базової, UAH):")
+
+        for curr, rate in BankAccount.exchange_rates.items():
+            print(f"  {curr}: {rate}")
+
+
+    def convert(self, amount, from_currency, to_currency):
+        """
+        Переводить суму з однієї валюти в іншу.
+        Формула: amount * (rate_from / rate_to)
+        """
+        from_curr = from_currency.upper()
+        to_curr = to_currency.upper()
+        # Перевірка валют
+        if from_curr not in BankAccount.exchange_rates or to_curr not in BankAccount.exchange_rates:
+            raise ValueError("1 або 2 валюти невідомі")
+
+
+        rate_from = BankAccount.exchange_rates[from_curr]
+        rate_to = BankAccount.exchange_rates[to_curr]
+
+        amount_in_base = amount * rate_from
+        converted_amount = amount_in_base / rate_to
+        return converted_amount
+
+    def change_currency(self, new_currency):
+
+        new_currency = new_currency.upper()
+        self.check_currency(new_currency)
+
+        if new_currency == self.currency:
+            print("Нова валюта співпадає з поточною. Зміни не потрібні.")
         else:
-            print("Стан проекту: незавершений")
+            # Перераховуємо баланс у нову валюту
+            new_balance = self.convert(self.balance, self.currency, new_currency)
+            print(f"Зміна валюти з {self.currency} на {new_currency}.")
+            print(f"Баланс: {self.balance:.2f} {self.currency} -> {new_balance:.2f} {new_currency}")
+            self.balance = new_balance
+            self.currency = new_currency
 
-            print('Задачі що залишились:')
-            for task in self.tasks:
-                print(f'   - {task}')
+    def deposit(self, amount):
+
+        if amount <= 0:
+            print("Сума для поповнення має бути позитивною.")
+        else:
+            self.balance += amount
+            print(f"Поповнення на {amount:.2f} {self.currency}. Новий баланс: {self.balance:.2f} {self.currency}")
+
+    def withdraw(self, amount):
+
+        if amount <= 0:
+            print("Сума для зняття має бути позитивною.")
+        elif amount > self.balance:
+            print("Недостатньо коштів для зняття.")
+        else:
+            self.balance -= amount
+            print(f"Зняття {amount:.2f} {self.currency}. Залишок: {self.balance:.2f} {self.currency}")
 
 
-project = Project(name='Ігрушка',
-                  budget=10_000,
-                  tasks=['Знайти інвесторів',
-                         'Придумати загальну ідею'])
+# Приклад :
+try:
+    account = BankAccount("Anna", 10030, "USD")
+    account.display_info()
 
-project.display_info()
+    print("\nПоповнення рахунку:")
+    account.deposit(2000)
 
-# Завдання 2
-# Створіть клас Телефон з атрибутами:
-#  максимальний обсяг пам’яті
-# Практичне завдання
-#  зайнята пам’ять
-#  чи включений(за замовчуванням False)
-#  встановлені додатки у вигляді словника, де ключ –
-# назва додатку, значення – обсяг пам’яті
-# Додайте методи:
-#  вивести інформацію про використання пам’яті
-#  видалити додаток
-#  встановити новий додаток, якщо пам’яті достатньо
-#  оновити додаток(нова версія може займати іншу
-# кількість пам’яті)
-#  запустити додаток, якщо він є і якщо телефон
-# вкючений
-#  включити телефон
-#  виключити телефон
+    print("\nЗняття коштів:")
+    account.withdraw(1530)
 
-class Phone:
-    def __init__(self, max_memory, apps):
-        self.max_memory = max_memory
-        self.is_on = False
-        self.apps = apps
+    print("\nПеревід валюти:")
+    account.change_currency("EUR")
 
-        self.used_memory = 0 # рахуємо память
-        for app_name in self.apps:
-            self.used_memory += self.apps[app_name]
+    print("\nСпроба з невідомою валютою:")
+    account.change_currency("ABC")  # ValueError
 
-    def display_info(self):
-        print(f'використана память {self.used_memory}/{self.max_memory}ГБ')
-        print(f'встановлені додатки')
-        for app_name in self.apps:
-            print(f'встановлений додаток {app_name} -{self.apps [app_name]} ГБ')
-        print()
-        print()
+except ValueError as ve:
+    print(f"Помилка: {ve}")
 
-    def delete_app(self, app_name):
-        if app_name not in self.apps:
-            print('немає такого додатку')
-            return
-
-        app_memory = self.apps.pop(app_name)
-        self.used_memory -= app_memory
-
-    def turn_on(self):
-        self.is_on = True
-
-    def turn_off(self):
-        self.is_on = False
-
-phone = Phone(128,
-              {'Google': 30,
-              'Ytube': 20,
-               'Telegram':5})
-
-phone.display_info()
-
-phone.delete_app('Ytube')
-
-phone.display_info()
