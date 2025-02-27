28c69255b705a77b241ad0c2beedb96e7567649d
annaaa11 <gosberex@gmail.com>
2025-02-27 17:06:47 +0200

    Тема: ООП. Частина 5 Створіть клас Pet з атрибутами  name – ім’я тварини  satiety – рівень ситості(від 0 до 100, за замовчуванням 50)  energy – рівень енергії (від 0 до 100, за замовчуванням 50) Методи:  sleep() – збільшує energy до 100  eat(food_amont) – їсть, збільшує satiety на food_amount  play(activity_level) – абстрактний метод  make_sound() – просто pass

--- main.py
+++ main.py
@@ -1,63 +1,73 @@
-# Створіть клас Recipe з атрибутами
-#  name – назва страви
-#  ingredients – список продуктів
-#  text – текст рецепту
-#  time – час приготування
 
 
-class Recipe:
-    def __init__(self, name, ingredients, text, time):
+class Pet:
+    def __init__(self, name, satiety=50, energy=50):
         self.name = name
-        self.ingredients = ingredients
-        self.text = text
-        self.time = time
 
- #   – повертає     назву     страви
-    def __str__(self):
-        return f"назва страви - {self.name} "
+        self.satiety = max(0, min(satiety, 100))  # Обмеження від 0 до 100
+        self.energy = max(0, min(energy, 100))  # Обмеження від 0 до 100
 
-    def __gt__(self, other): # – перевіряє чи є час приготування self більшим за other
-        return self.time > other.time
+    def sleep(self):
+        self.energy = 100
 
-    #  перевіряє     чи     є     інгредієнт     в     рецепті
-    def __contains__(self, ingredient): #
-        return ingredient in self.ingredients
+    def eat(self, food_amont):
+        self.satiety += food_amont
 
-    def display_info(self): # виводить всю інформацію про рецепт
-        print (f"назва страви: {self.name}")
-        print(f"список продуктів: {self.ingredients}")
-        print(f"текст рецепту: {self.text}")
-        print(f"час приготування: {self.time}  хвилин")
+    def play(activity_level):
+        pass
+
+    def make_sound(self):
+        pass
+
+class Cat(Pet):
+
+    def play(self, activity_level):     #якщо    satiety > 60, зменшує    energy     на
+        if self.satiety > 60:
+            self.satiety -= activity_level
+            self.energy -= 2*activity_level
+            print(f"рівень ситості= {self.satiety} рівень енергії= {self.energy} ")
+
+    def make_sound(self):
+        print("Мяу")
+
+    def catch_mouse(self):
+        if self.energy > 30:
+            print("ловить мишу")
+        if self.satiety > 40:
+            print("грається з мишею")
+        else:
+            print("їсть")
+
+class Dog(Pet):
+    def play(self, activity_level):
+        if self.satiety >15:
+            self.satiety -= activity_level//2
+            self.energy -= activity_level//2
+        print(f"рівень ситості= {self.satiety} рівень енергії= {self.energy} ")
+
+    def make_sound(self):
+        print("Гав")
+
+    def fetch_ball(self):
+        print("ловить м’яча")
+        if self.satiety > 10:
+            self.energy -= 5
+
+dog1 = Dog("Bim")
+dog1.make_sound()
+dog1.play(20)
+dog1.fetch_ball()
+
+cat1 = Cat("mura")
+cat1.make_sound()
+cat1.eat(30)
+cat1.sleep()
+cat1.catch_mouse()
+cat1.play(30)
 
 
-#Приклад рецептів:
-Recipe1 = Recipe("Піца",
-["борошно", "вода", "дріжджі", "томат", "сир"],
-"Готуємо тісто, додаємо інгредієнти та запікаємо",
-30)
-
-Recipe2 = Recipe("Салат",
-       ["томат", "огірок", "зелень", "олія"],
-       "Нарізаємо овочі, додаємо зелень та поливаємо олією ",
-10)
-
-Recipe3 = Recipe("Суп",
-       ["вода", "картопля", "морква", "м'ясо"],
-       "Варимо всі інгредієнти до готовності",
-       45)
-
-recipes = [Recipe1, Recipe2, Recipe3]
 
 
-# Виведення рецептів з інгредієнтом "томат"
-print("Рецепти з томатом:")
-for recipe in recipes:
-    if "томат" in recipe:
-        print(f"- {recipe}")
-
-# Рецепт з найменшим часом приготування
-fastest_recipe = min(recipes)
 
 
-print("\nРецепт з найменшим часом приготування:")
-fastest_recipe.display_info()
+
