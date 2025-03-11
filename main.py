08a6064605bbb61b531e906737ea55ace02990b2
annaaa11 <gosberex@gmail.com>
2025-03-11 17:12:10 +0200

    Створіть клас Project # Атрибути: #  tasks – стек з завданнями, об’єкти класу Task ( початкове # завдання передається в init) # Методи: #  do_task() – видалити останнє завдання з стеку та # виконати його, якщо для цього потрібно зробити під # завдання, то добавити їх у стек # Якщо стек порожній то вивести про це повідомлення #  is_finished() – True якщо завдань не залишилось

--- main.py
+++ main.py
@@ -1,154 +1,233 @@
-# Використовуючи класи з практичної реалізуйте клас Shop з
-# трьома чергами до кас. Кожна черга реалізується через
-# двозв’язний список
-# Атрибути
-#  queue1, queue2, queue3 – черги до кас
-# Методи
-#  add_buyer(name, idx) – додає покупця в кінець черги
-# номер idx
-#  serve_buyer(idx) – обслуговує покупця з черги
-# idx(вивести повідомлення та видалити покупця з черги)
-# Якщо черга стала порожньою, то викликати _reorder(idx)
-#  _reorder(idx) – з усіх черг останній покупець переходить
-# в чергу з номером idx
-#  display_info() – виводить на екран 3 черги
 
+# Використовуючи стеки, змоделюйте роботу над
+# виконанням проекту. Як відомо складні завдання часто
+# розбивають на під задачі в процесі роботи, і тільки коли всі
+# вони виконані вважається що з основним завданням ви
+# впорались.
+
+# DoubleLinkedList COPY:
 class Node:
     def __init__(self, data):
-        self.data = data  # Значення вузла
-        self.next = None  # Посилання на наступний вузол
-        self.prev = None  # Посилання на попередній вузол
+        self.data = data
+        self.next = None
+        self.prev = None
 
     def __str__(self):
         return f"{self.data} -> {self.next}"
 
-#двозв’язний список
-class DoublyLinkedList:
+
+class DoubleLinkedList:
+    """
+    Клас двозв'язного списку.
+    """
+
     def __init__(self):
+        """
+        Ініціалізація порожнього списку.
+        """
         self.head = None
         self.tail = None
 
     def __str__(self):
-        return f"{self.head}"
+        return str(self.head)
 
-    def push_end(self, data): #додає new_node в кінець self.tail
+    def push_end(self, data):
+        """
+        Додає елемент у кінець списку.
+        :param data: Дані для додавання
+        """
         new_node = Node(data)
-        if self.head is None: # Перевірка чи список порожній
+        if not self.head:
             self.head = new_node
             self.tail = new_node
-            return
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
 
-        self.tail.next = new_node
-        new_node.prev = self.tail
-        self.tail = new_node
+    def push_start(self, data):
+        """
+        Додає елемент на початок списку.
+        :param data: Дані для додавання
+        """
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
-    def pop_start(self): # удаляем начало
+    def pop_end(self):
+        """
+        Видаляє останній елемент зі списку.
+        :return: Дані видаленого елемента або None, якщо список порожній
+        """
+        if not self.tail:
+            return None
 
-        if self.head is None:
-            print("список порожній")
-            return
+        data = self.tail.data
 
         if self.head.next is None:
             self.head = None
             self.tail = None
-            return
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
 
-        self.head = self.head.next
-        self.head.prev = None
+        return data
 
-    def pop_end(self): # удаляем последнего
+    def pop_start(self):
+        """
+        Видаляє перший елемент зі списку.
+        :return: Дані видаленого елемента або None, якщо список порожній
+        """
 
-        if self.tail is None:
-            print("список порожній")
-            return
-        if self.tail.prev is None: # 1 node
+        if not self.head:
+            return None
+
+        data = self.head.data
+
+        if self.head.next is None:
             self.head = None
             self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        return data
+
+    def is_empty(self):
+        """
+        Чи є порожній
+        :return: True якщо порожній
+        """
+        return self.head is None
+
+    def peek(self):
+        """
+        Повертає останній елемент, не видаляючи його
+        :return: останній елемент
+        """
+        return self.tail.data
+
+
+
+# Клас Task(уже реалізований):
+#  do() – виконує завдання(виводить на екран інформацію
+# про це) та повертає список з підзавданням для
+# успішного виконаня
+
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.subtasks = []
+
+    def do(self):
+        """
+        Виконує завдання, за потреби розбиває його на підзавдання
+        :return: список підзавдань
+        """
+        if self.subtasks:
+            print(f"Виконую завдання: {self.name}. Розбиваю на підзавдання")
+        else:
+            print(f"Завершено завдання: {self.name}")
+
+        return self.subtasks # список
+
+
+# Створіть клас Project
+# Атрибути:
+#  tasks – стек з завданнями, об’єкти класу Task ( початкове
+# завдання передається в init)
+# Методи:
+#  do_task() – видалити останнє завдання з стеку та
+# виконати його, якщо для цього потрібно зробити під
+# завдання, то добавити їх у стек
+# Якщо стек порожній то вивести про це повідомлення
+#  is_finished() – True якщо завдань не залишилось
+
+class Project:
+    ## MY CODE
+
+    def __init__(self, initial_task):
+        self.tasks = DoubleLinkedList()
+        self.tasks.push_end(initial_task)
+        #tasks – стек з завданнями, об’єкти класу Task
+        # (initial_task - початкове  завдання передається в init)
+
+    def do_task(self):
+        if self.tasks.is_empty():
+            print("Усі завдання виконано!")
             return
 
-        self.tail = self.tail.prev
-        self.tail.next = None
+        current_task = self.tasks.pop_end() #  current_task - объект класса Task
+        new_tasks = current_task.do() # запуск метода do() класса Task
 
-class Shop:
-    def __init__(self):
-       # queue1, queue2, queue3 – черги         до         кас
-        self.queue1 = DoublyLinkedList()
-        self.queue2 = DoublyLinkedList()
-        self.queue3 = DoublyLinkedList()
+        for task in new_tasks:
+            self.tasks.push_end(task)
 
-    def display_info(self):
-
-        print(f"черга 1: {self.queue1}")
-        print(f"черга 2: {self.queue2}")
-        print(f"черга 3: {self.queue3}")
+    def is_finished(self):
+        return self.tasks.is_empty()
+##################
 
 
-    def add_buyer(self, name, idx):
-        if idx == 1:
-            self.queue1.push_end(name)
-        elif idx == 2:
-            self.queue2.push_end(name)
-        elif idx == 3:
-            self.queue3.push_end(name)
-        else:
-            print("черги до кас: 1,2,3!")
+task = Task('Підготовка до зйомок')
 
-    def _reorder(self, idx): # з усіх черг останній покупець переходить
-# в чергу з номером idx
-        last_buyer = None
-        for queue in [self.queue3, self.queue2, self.queue1]:
-            if queue.tail:
-                last_buyer = queue.tail
-                queue.pop_end()
-                #print(last_buyer)
-                break
+task.subtasks = [
+    Task('Пошук локацій'),
+    Task('Підготовка сценарію'),
+    Task('Кастинг акторів')
+]
 
-        if last_buyer:
-            self.add_buyer(last_buyer, idx)
+# Підзавдання для "Пошук локацій"
+task.subtasks[0].subtasks = [
+    Task('Огляд локацій у місті'),
+    Task('Огляд локацій за містом'),
+    Task('Узгодження місць для зйомок')
+]
 
-    def serve_buyer(self, idx): #oбслуговує покупця з черги idx
-        if idx == 1:
-            if self.queue1.head:
-                print(f"oбслуговує покупця {self.queue1.head.data} з черги1")
-                self.queue1.pop_start() # видалити покупця з черги
-                if self.queue1.head is None: # Якщо черга стала порожньою, то викликати _reorder(idx)
-                    self._reorder(1)
-        elif idx == 2:
-            if self.queue2.head:
-                print(f"oбслуговує покупця {self.queue2.head.data} з черги2")
-                self.queue2.pop_start()
-                if not self.queue2.head:
-                    self._reorder(2)
-        elif idx == 3:
-            if self.queue3.head:
-                print(f"oбслуговує покупця {self.queue3.head.data} з черги3")
-                self.queue3.pop_start()
-                if not self.queue3.head:
-                    self._reorder(3)
-        else:
-            print("черги до кас: 1,2,3!")
+# Підзавдання для "Підготовка сценарію"
+task.subtasks[1].subtasks = [
+    Task('Написання основного сценарію'),
+    Task('Редагування сценарію'),
+    Task('Підготовка сценарних приміток'),
+]
 
-# Приклад використання
-shop = Shop()
-shop.add_buyer("Олег", 1)
-shop.add_buyer("Марина", 2)
-shop.add_buyer("Марія", 2)
-shop.add_buyer("Андрій", 3)
-shop.add_buyer("Ірина", 1)
-shop.add_buyer("Василь", 2)
-shop.add_buyer("Тетяна", 3)
-shop.add_buyer("Сергій", 3)
-shop.add_buyer("Анна", 3)
-print("Черги:")
-shop. display_info()
+# Підзавдання для "Кастинг акторів"
+task.subtasks[2].subtasks = [
+    Task('Пошук головних акторів'),
+    Task('Пошук другорядних акторів'),
+    Task('Підготовка контрактів для акторів')
+]
 
-shop.serve_buyer(1)
-shop.serve_buyer(2)
-shop.serve_buyer(3)
-print("Після обслуговування покупців:")
-shop. display_info()
+# Підзавдання для "Пошук локацій у місті"
+task.subtasks[0].subtasks[0].subtasks = [
+    Task('Вибір декорацій для зйомок'),
+    Task('Узгодження з власниками приміщень')
+]
 
-shop.serve_buyer(1)
-print("Покупці перейшли до вільної каси:")
-shop.display_info()
+# Підзавдання для "Огляд локацій за містом"
+task.subtasks[0].subtasks[1].subtasks = [
+    Task('Вибір лісу для сцени битви'),
+    Task('Пошук старовинних будівель для сцени'),
+]
 
+# Підзавдання для "Написання основного сценарію"
+task.subtasks[1].subtasks[0].subtasks = [
+    Task('Написання першої частини'),
+    Task('Написання другої частини'),
+]
+
+# Підзавдання для "Пошук головних акторів"
+task.subtasks[2].subtasks[0].subtasks = [
+    Task('Пошук актора на роль головного героя'),
+    Task('Пошук актриси на роль головної героїні')
+]
+
+project = Project(task) # task - задание класса Task
+
+while not project.is_finished():
+    project.do_task()
+
+######
