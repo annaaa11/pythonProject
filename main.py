227f63f22721af8e3eb187b9c4eb0b78c1c7482e
annaaa11 <gosberex@gmail.com>
2025-03-13 21:30:46 +0200

    # Створіть програму роботи зі словником. Наприклад, # англо-іспанський, французько-німецький або інша мовна пара. # Програма має: #  надавати початкове введення даних для словника #  відображати слово та його переклади #  дозволяти додавати, змінювати, видаляти переклади # слов

--- main.py
+++ main.py
@@ -1,125 +1,187 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.left = None
+#         self.right = None
 #
 #
-# # Створіть дочірні класи від Zone та перевизначте метод
-# # serve_passenger() щоб він повертав пару: пасажир та True/False
-# # в залежності від успішності перевірки.
-# # Перевірки:
-# #  реєстрація – наявність білету(у багажі)
-# #  безпека – відсутність небезпечних предметів у багажі:
-# # ніж, зброя, вибухівка
-# #  посадка – перевірка не потрібна
-# # Для цього скористайтесь класом Passenger
-# # Атрибути:
-# #  name – ім’я
-# #  priority – пріоритет
-# #  baggage – список з предметами в багажі
+# class BinaryTree:
+#     def __init__(self):
+#         self.root = None
 #
+#     def add(self, data):
+#         node = Node(data)
+#
+#         if self.root is None:
+#             self.root = node
+#             return
+#
+#         self._recursive_add(self.root, node)
+#
+#     def _recursive_add(self, tree_node, added_node):
+#         if added_node.data < tree_node.data:
+#             # рухаємося наліво
+#
+#             # перевіряємо чи пусто зліва
+#             if tree_node.left is None:
+#                 tree_node.left = added_node
+#                 return  # рекурсія(цикл) закінчується
+#
+#             # якщо не пусто
+#             self._recursive_add(tree_node.left, added_node)
+#         else:
+#             # рухаємося направо
+#
+#             # перевіряємо чи пусто справа
+#             if tree_node.right is None:
+#                 tree_node.right = added_node
+#                 return  # рекурсія(цикл) закінчується
+#
+#             # якщо не пусто
+#             self._recursive_add(tree_node.right, added_node)
+#
+#     def display(self):
+#         nodes_to_display = [self.root]
+#
+#         while len(nodes_to_display) != 0:
+#             # відображаємо все що є в nodes_to_display
+#             for node in nodes_to_display:
+#                 print(node.data, end=' ')
+#             print()
+#
+#             # отримуємо наступні вузли
+#             new_nodes = []
+#             for node in nodes_to_display:
+#                 if node.left is not None:
+#                     new_nodes.append(node.left)
+#
+#                 if node.right is not None:
+#                     new_nodes.append(node.right)
+#
+#             # замінюємо nodes_to_display на нові вузли
+#             nodes_to_display = new_nodes
+#
+#     def get_min(self):
+#         # отримати найменший елемент
+#
+#         node = self.root
+#
+#         while node.left is not None:
+#             node = node.left
+#
+#         return node.data
+#
+#     def find(self, num):
+#         # якщо num в корені
+#         if self.root.data == num:
+#             return True
+#
+#         return self._recursive_find(self.root, num)
+#
+#     def _recursive_find(self, tree_node, num):
+#         # числа нема
+#         if tree_node is None:
+#             return False
+#
+#         # число знайдене
+#         if tree_node.data == num:
+#             return True
+#
+#         if num < tree_node.data:
+#             return self._recursive_find(tree_node.left, num)
+#         else:
+#             return self._recursive_find(tree_node.right, num)
+#
+#
+# tree = BinaryTree()
+# tree.add(5)
+# tree.add(4)
+# tree.add(6)
+# tree.add(3)
+# tree.add(2)
+# tree.add(1)
+# tree.add(0)
+# # tree.display()
+# print(tree.get_min())
+# print(tree.find(1))
+# print(tree.find(10))
 
-from queue import PriorityQueue
+# import bintrees
+#
+# class Node:
+#     def __init__(self, data):
+#         self.key = data  # вирішуємо наліво чи направо
+#         self.value = None # додаткова інформація
+#         self.left = None
+#         self.right = None
+#
+#
+# tree = bintrees.AVLTree()
+#
+# tree.insert(key=10, value='apple')
+# tree.insert(key=11, value='orange')
+# tree.insert(key=5, value='pear')
+# tree.insert(key=9, value='melon')
+# tree.insert(key=20, value='banana')
+#
+#
+# # дістати елемент з id 5
+# print(tree[5])
+#
+# # ключі str
+#
+# tree = bintrees.AVLTree()
+#
+# tree.insert(key='apple', value='sweet juicy apple ')
+# tree.insert(key='orange', value='orange')
+# tree.insert(key='pear', value='pear')
+# tree.insert(key='melon', value='melon')
+# tree.insert(key='banana', value='banana')
+#
+# print(tree['apple'])
 
-class Passenger:
-    def __init__(self, name, priority, baggage):
+# Створіть програму роботи зі словником. Наприклад,
+# англо-іспанський, французько-німецький або інша мовна пара.
+# Програма має:
+#  надавати початкове введення даних для словника
+#  відображати слово та його переклади
+#  дозволяти додавати, змінювати, видаляти переклади
+# слов
+
+from bintrees import AVLTree
+
+
+class Word:
+    def __init__(self, name, translation):
         self.name = name
-        self.priority = priority
-        self.baggage = baggage  # список предметів у багажі
+        self.translations = [translation]
 
 
-class Zone:
-    def __init__(self, name):
-        self.name = name
-        self.passengers = PriorityQueue()
-
-    def add_passenger(self, passenger):
-        priority = passenger.priority
-        pair = (priority, passenger)
-        self.passengers.put(pair)
-
-    def serve_passenger(self):
-        #if not self.passengers.empty():
-        priority, passenger = self.passengers.get()
-        return passenger
-        #return None
-
-
-class RegistrationZone(Zone):
-    def serve_passenger(self):
-        #if not self.passengers.empty():
-        priority, passenger = self.passengers.get()
-            # Перевірка наявності квитка в багажі
-        has_ticket = "ticket" in passenger.baggage
-        return passenger, has_ticket
-        #return None, False
-
-
-class ControlZone(Zone):
-    def serve_passenger(self):
-        if not self.passengers.empty():
-            priority, passenger = self.passengers.get()
-            # Перевірка на небезпечні предмети
-            dangerous_items = ["knife", "weapon", "explosive"]
-            for item in passenger.baggage:
-                if item in dangerous_items:
-                    return passenger, False
-            return passenger, True
-        return None, False
-
-
-class BoardingZone(Zone):
-    def serve_passenger(self):
-        if not self.passengers.empty():
-            priority, passenger = self.passengers.get()
-            # Для посадки перевірка не потрібна
-            return passenger, True
-        return None, False
-
-
-class Airport:
+class Dict:
     def __init__(self):
-        self.zones = {
-            "Registration": RegistrationZone("Реєстрація"),
-            "Control": ControlZone("Контроль"),
-            "Board": BoardingZone("Посадка")
-        }
-        self.passengers = []
+        self.tree_words = AVLTree()
 
-    def add(self, passenger):
-        self.zones["Registration"].add_passenger(passenger)
+    def add_word(self, name, translation):
+        if name in self.tree_words:
+            word = self.tree_words[name]
+            word.translations.append(translation)
+        else:
+            new_word = Word(name, translation)
+            self.tree_words.insert(name, new_word)
 
-    def serve_registration(self):
-        passenger, has_ticket = self.zones["Registration"].serve_passenger()
-        if passenger and has_ticket:
-            self.zones["Control"].add_passenger(passenger)
-
-    def serve_security_control(self):
-        passenger, passed_security = self.zones["Control"].serve_passenger()
-        if passenger and passed_security:
-            self.zones["Board"].add_passenger(passenger)
-
-    def serve_boarding(self):
-        passenger, ch = self.zones["Board"].serve_passenger()
-        if passenger:
-            self.passengers.append(passenger)
-
-    def show_statistics(self):
-        print(f"Кількість пасажирів, які пройшли всі зони: {len(self.passengers)}")
-        print("Список пасажирів на борту:")
-        for p in self.passengers:
-            print(f"Пасажир {p.name}")
+    def get(self, name):
+        return self.tree_words[name]
 
 
-# Тестування
-airport = Airport()
-passengers = [
-    Passenger("Олег", 3, ["ticket", "clothes"]),
-    Passenger("Анна", 1, ["ticket", "knife"]),
-    Passenger("Марія", 4, ["clothes"]),  # немає квитка
-    Passenger("Сергій", 2, ["ticket", "book"])
-]
+my_dict = Dict()
+my_dict.add_word("run", "бігати")
+my_dict.add_word("run", "робити")
+run = my_dict.get("run")
+print(run.translations)
 
-for p in passengers:
-    airport.add(p)
-    airport.serve_registration()
-    airport.serve_security_control()
-    airport.serve_boarding()
 
-airport.show_statistics()
+
+
+
+
+
