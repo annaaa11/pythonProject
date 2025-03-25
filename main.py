fe167f1c28d759818cd7567a89d1f78c22377f3b
annaaa11 <gosberex@gmail.com>
2025-03-25 18:04:40 +0200

    Використовуючи бінарні дерева, організуйте роботу автопарку, де зберігаються автомобілі, відсортовані за маркою

--- main.py
+++ main.py
@@ -1,187 +1,92 @@
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.left = None
-#         self.right = None
-#
-#
-# class BinaryTree:
-#     def __init__(self):
-#         self.root = None
-#
-#     def add(self, data):
-#         node = Node(data)
-#
-#         if self.root is None:
-#             self.root = node
-#             return
-#
-#         self._recursive_add(self.root, node)
-#
-#     def _recursive_add(self, tree_node, added_node):
-#         if added_node.data < tree_node.data:
-#             # рухаємося наліво
-#
-#             # перевіряємо чи пусто зліва
-#             if tree_node.left is None:
-#                 tree_node.left = added_node
-#                 return  # рекурсія(цикл) закінчується
-#
-#             # якщо не пусто
-#             self._recursive_add(tree_node.left, added_node)
-#         else:
-#             # рухаємося направо
-#
-#             # перевіряємо чи пусто справа
-#             if tree_node.right is None:
-#                 tree_node.right = added_node
-#                 return  # рекурсія(цикл) закінчується
-#
-#             # якщо не пусто
-#             self._recursive_add(tree_node.right, added_node)
-#
-#     def display(self):
-#         nodes_to_display = [self.root]
-#
-#         while len(nodes_to_display) != 0:
-#             # відображаємо все що є в nodes_to_display
-#             for node in nodes_to_display:
-#                 print(node.data, end=' ')
-#             print()
-#
-#             # отримуємо наступні вузли
-#             new_nodes = []
-#             for node in nodes_to_display:
-#                 if node.left is not None:
-#                     new_nodes.append(node.left)
-#
-#                 if node.right is not None:
-#                     new_nodes.append(node.right)
-#
-#             # замінюємо nodes_to_display на нові вузли
-#             nodes_to_display = new_nodes
-#
-#     def get_min(self):
-#         # отримати найменший елемент
-#
-#         node = self.root
-#
-#         while node.left is not None:
-#             node = node.left
-#
-#         return node.data
-#
-#     def find(self, num):
-#         # якщо num в корені
-#         if self.root.data == num:
-#             return True
-#
-#         return self._recursive_find(self.root, num)
-#
-#     def _recursive_find(self, tree_node, num):
-#         # числа нема
-#         if tree_node is None:
-#             return False
-#
-#         # число знайдене
-#         if tree_node.data == num:
-#             return True
-#
-#         if num < tree_node.data:
-#             return self._recursive_find(tree_node.left, num)
-#         else:
-#             return self._recursive_find(tree_node.right, num)
-#
-#
-# tree = BinaryTree()
-# tree.add(5)
-# tree.add(4)
-# tree.add(6)
-# tree.add(3)
-# tree.add(2)
-# tree.add(1)
-# tree.add(0)
-# # tree.display()
-# print(tree.get_min())
-# print(tree.find(1))
-# print(tree.find(10))
-
-# import bintrees
-#
-# class Node:
-#     def __init__(self, data):
-#         self.key = data  # вирішуємо наліво чи направо
-#         self.value = None # додаткова інформація
-#         self.left = None
-#         self.right = None
-#
-#
-# tree = bintrees.AVLTree()
-#
-# tree.insert(key=10, value='apple')
-# tree.insert(key=11, value='orange')
-# tree.insert(key=5, value='pear')
-# tree.insert(key=9, value='melon')
-# tree.insert(key=20, value='banana')
-#
-#
-# # дістати елемент з id 5
-# print(tree[5])
-#
-# # ключі str
-#
-# tree = bintrees.AVLTree()
-#
-# tree.insert(key='apple', value='sweet juicy apple ')
-# tree.insert(key='orange', value='orange')
-# tree.insert(key='pear', value='pear')
-# tree.insert(key='melon', value='melon')
-# tree.insert(key='banana', value='banana')
-#
-# print(tree['apple'])
-
-# Створіть програму роботи зі словником. Наприклад,
-# англо-іспанський, французько-німецький або інша мовна пара.
-# Програма має:
-#  надавати початкове введення даних для словника
-#  відображати слово та його переклади
-#  дозволяти додавати, змінювати, видаляти переклади
-# слов
-
+"""
+Використовуючи бінарні дерева, організуйте роботу
+автопарку, де зберігаються автомобілі, відсортовані за
+маркою
+Клас Car
+Атрибути:
+ brand – модель автомобіля
+ model – марка автомобіля
+ year – рік випуску
+Клас CarPark
+Атрибути:
+ cars – дерево з автомобілями
+Методи:
+ add(car) – добавити автомобіль
+ remove(model) – видалити автомобіль
+ search(model) – пошук автомобіля за маркою, якщо є
+то повертає книгу інакше None
+ __len__() – кількість автомобілів
+ sell_car(client, model) – продати автомобіль клієнту,
+якщо така марка присутня
+"""
+import bintrees
 from bintrees import AVLTree
 
+class Car:
+    def __init__(self, brand , model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
 
-class Word:
-    def __init__(self, name, translation):
-        self.name = name
-        self.translations = [translation]
+# Атрибути:
+#  brand – модель автомобіля
+#  model – марка автомобіля
+#  year – рік випуску
 
-
-class Dict:
+class CarPark:
     def __init__(self):
-        self.tree_words = AVLTree()
+        self.cars = bintrees.AVLTree()    # cars – дерево з     автомобілями
 
-    def add_word(self, name, translation):
-        if name in self.tree_words:
-            word = self.tree_words[name]
-            word.translations.append(translation)
+    def add(self, car):
+        self.cars.insert(key=car.model, value=car)
+
+    def remove(self, model):
+        if model in self.cars:
+            self.cars.remove(model)
         else:
-            new_word = Word(name, translation)
-            self.tree_words.insert(name, new_word)
+            print(f'автомобіля "{model}" немає!')
 
-    def get(self, name):
-        return self.tree_words[name]
+    def search(self, model):   #пошук автомобіля за маркою
 
+        if model in self.cars:
+            return self.cars[model]
+        else:
+            return None
 
-my_dict = Dict()
-my_dict.add_word("run", "бігати")
-my_dict.add_word("run", "робити")
-run = my_dict.get("run")
-print(run.translations)
+    def __len__(self): #кількість автомобілів
+        return len(self.cars)
 
+    def sell_car(self, client, model): #продати автомобіль клієнту,
+#якщо така марка присутня
+        car = self.search(model)
+        if not car:
+            print(f'автомобіля "{model}" немає!')
+            return
+        self.remove(model)
 
+        print(f'Клієнт {client} купив автомобіль  {model}')
 
+    def display_info(self, model):
+        car = self.search(model)
+        print(f'''
+модель автомобіля - {car.brand}
+марка автомобіля - {car.model}
+рік випуску - {car.year}
+''')
 
+park_cars = CarPark()
+cars = [
+    Car("x032", "BMW", 2003),
+    Car("nn889", "Audi", 2022),
+    Car("nn8890", "Cherry", 2021),
+]
 
+for car in cars:
+    park_cars.add(car)
 
+print(f"📚 кількість автомобілів {len(park_cars)} автопарку ")
+park_cars.display_info("Audi")
+park_cars.sell_car("Anna","Audi")
+#park_cars.display_info("Audi")
 
+print(f"📚 кількість автомобілів {len(park_cars)} автопарку ")
